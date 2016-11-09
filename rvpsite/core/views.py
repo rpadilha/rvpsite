42c67716c52e77126e7da46bb36074ae0299697e
Renato Padilha <padilha@renatoair.local>
2016-11-09 09:20:53 -0200

    HTML Change

--- rvpsite/core/views.py
+++ rvpsite/core/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+
+def area(request):
+    return render(request, 'area.html')
+
+
+def representations(request):
+    return render(request, 'representadas.html')
+
+
+def register(request):
+    return render(request, 'cadastro.html')
--- rvpsite/urls.py
+++ rvpsite/urls.py
@@ -4,5 +4,8 @@ from rvpsite.core import views as rvpsite_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', rvpsite_views.home),
+    url(r'^$', rvpsite_views.home, name='home'),
+    url(r'^area/', rvpsite_views.area, name='area'),
+    url(r'^representadas/', rvpsite_views.representations, name='representations'),
+    url(r'^cadastro/', rvpsite_views.register, name='register'),
 ]
