d2ea252fbf259bbfc01b62481896af1bd1437667
Renato Padilha <tonare@gmail.com>
2017-03-03 11:13:58 -0300

    Promo Março v2

--- rvpsite/urls.py
+++ rvpsite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.http import HttpResponse
 from rvpsite.core import views as rvpsite_views
 from rvpsite.contact.views import contact
-from rvpsite.blog.views import blog
+#from rvpsite.blog.views import blog
 from rvpsite.client.views import clients, clients_tmp
 
 urlpatterns = [
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^clientes/$', clients, name='clients'),
     url(r'^clientes_novo/$', clients_tmp, name='clients_novo'), # desenvolvimento
     url(r'^noticias/$', rvpsite_views.blog, name='blog'),
-    url(r'^blog/(?P<slug>[\w-]+)/$', blog, name='blog_novo'), # desenvolvimento
+    #url(r'^blog/(?P<slug>[\w-]+)/$', blog, name='blog_novo'), # desenvolvimento
     url(r'^contato/$', contact, name='contact'),
     url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow: /clientes/\nDisallow: /clientes_novo/\nDisallow: /blog/*",
                                                content_type="text/plain"), name="robots_file"),
